c90994bf072db6bd1a0ede793a12e3d8911ac506
Darya Matveeva <darya.d.chernikova@gmail.com>
2024-06-26 20:42:12 +0300

    bounded stack 2

--- src/main/java/boundedstack/AbstractBoundedStack.java
+++ src/main/java/boundedstack/AbstractBoundedStack.java
@@ -1,4 +1,48 @@
 package boundedstack;
 
-public class AbstractBoundedStack {
+public abstract class AbstractBoundedStack<T> {
+    public final int POP_NIL = 0; // push() ещё не вызывалась
+    public final int POP_OK = 1; // последняя pop() отработала нормально
+    public final int POP_ERR = 2; // стек пуст
+
+    public final int PEEK_NIL = 0; // peek() ещё не вызывалась
+    public final int PEEK_OK = 1; // последняя peek() вернула корректное значение
+    public final int PEEK_ERR = 2; // стек пуст
+
+    public final int PUSH_NIL = 0; // push() ещё не вызывалась
+    public final int PUSH_OK = 1; // последняя push() отработала нормально
+    public final int PUSH_ERR = 2; // количество элементов в стеке = MAX_SIZE
+
+    // предусловие: аргумент maxSize > 0
+    // постусловие: создан новый пустой стек с максимальным размером maxSize
+    public AbstractBoundedStack(int maxSize) {}
+
+    // постусловие: создан новый пустой стек с максимальным размером 32
+    public AbstractBoundedStack() {}
+
+    // предусловие: в стеке количество элементов меньше чем максимальный размер
+    // постусловие: в стек добавлено новое значение
+    public abstract void push(T value);
+
+    // предусловие: стек не пустой;
+    // постусловие: из стека удалён верхний элемент
+    public abstract void pop();
+
+    // постусловие: из стека удалятся все значения
+    public abstract void clear();
+
+    // предусловие: стек не пустой
+    public abstract T peek();
+
+    public abstract int size();
+
+    public abstract int getPopStatus(); // возвращает значение POP_*
+    public abstract int getPeekStatus(); // возвращает значение PEEK_*
+    public abstract int getPushStatus(); // возвращает значение PUSH_*
+
+    public abstract int getMaxSize(); // возвращает максимальный размер стека
 }
+
+
+
+
--- src/main/java/boundedstack/BoundedStack.java
+++ src/main/java/boundedstack/BoundedStack.java
@@ -1,4 +1,93 @@
 package boundedstack;
 
-public class BoundedStack {
+import java.util.LinkedList;
+import java.util.List;
+
+public class BoundedStack<T> extends AbstractBoundedStack<T> {
+
+    private List<T> stack; // основное хранилище стека
+    private int peekStatus; // статус запроса peek()
+    private int popStatus; // статус команды pop()
+    private int pushStatus; // статус команды push()
+    private final int maxSize;
+
+    public BoundedStack(int maxSize) {
+        if (maxSize > 0) {
+            this.maxSize = maxSize;
+        } else {
+            this.maxSize = 32;
+        }
+        clear();
+    }
+
+    public BoundedStack() {
+        this.maxSize = 32;
+        clear();
+    }
+
+    @Override
+    public void push(T value) {
+        if (size() < this.maxSize) {
+            stack.add(value);
+            pushStatus = PUSH_OK;
+        } else {
+            pushStatus = PUSH_ERR;
+        }
+    }
+
+    @Override
+    public void pop() {
+        if (size() > 0) {
+            stack.remove(size() - 1);
+            popStatus = POP_OK;
+        } else {
+            popStatus = POP_ERR;
+        }
+    }
+
+    @Override
+    public void clear() {
+        stack = new LinkedList<>();
+        peekStatus = PEEK_NIL;
+        popStatus = POP_NIL;
+        pushStatus = PUSH_NIL;
+    }
+
+    @Override
+    public T peek() {
+        T result;
+        if (size() > 0) {
+            result = stack.get(size() - 1);
+            peekStatus = PEEK_OK;
+        } else {
+            result = null;
+            peekStatus = PEEK_ERR;
+        }
+        return result;
+    }
+
+    @Override
+    public int size() {
+        return stack.size();
+    }
+
+    @Override
+    public int getPopStatus() {
+        return popStatus;
+    }
+
+    @Override
+    public int getPeekStatus() {
+        return peekStatus;
+    }
+
+    @Override
+    public int getPushStatus() {
+        return pushStatus;
+    }
+
+    @Override
+    public int getMaxSize() {
+        return maxSize;
+    }
 }
